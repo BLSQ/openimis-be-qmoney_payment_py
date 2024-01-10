9d3da562dbf3cd37de49c666333cb8af50d5e501
Christophe Philemotte <1059797+toch@users.noreply.github.com>
2024-01-10 15:41:07 +0100

    Format python code, refactor, and cover more edge cases for verifyCode API

--- qmoney_payment/tests/conftest.py
+++ qmoney_payment/tests/conftest.py
@@ -1,47 +1,72 @@
 import os
 import pytest
 
-from helpers import QMoney
+from simplegmail import Gmail
+
+from helpers import QMoney, gmail_mark_as_read_recent_emails_with_qmoney_otp
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope='session')
 def qmoney_url():
-    return os.getenv('QMONEY_URL')
+  return os.getenv('QMONEY_URL')
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope='session')
 def qmoney_credentials():
-    return (os.getenv('QMONEY_USERNAME'), os.getenv('QMONEY_PASSWORD'))
+  return (os.getenv('QMONEY_USERNAME'), os.getenv('QMONEY_PASSWORD'))
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope='session')
 def qmoney_token():
-    return os.getenv('QMONEY_TOKEN')
+  return os.getenv('QMONEY_TOKEN')
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope='session')
+def qmoney_payer():
+  return os.getenv('QMONEY_PAYER')
+
+
+@pytest.fixture(scope='session')
+def qmoney_payee():
+  return os.getenv('QMONEY_PAYEE')
+
+
+@pytest.fixture(scope='session')
+def qmoney_payee_pin_code():
+  return os.getenv('QMONEY_PAYEE_PIN_CODE')
+
+
+@pytest.fixture(scope='function')
 def qmoney_getmoney_json_payload():
-    return {
-        'data': {
-            'fromUser': {
-                'userIdentifier': '5811724c',
-            },
-            'toUser': {
-                'userIdentifier': '14001502'
-            },
-            'serviceId': 'MOBILE_MONEY',
-            'productId': 'NHIA_GETMONEY',
-            'remarks': 'add',
-            'payment': [
-                {
-                    'amount': 1000
-                },
-            ],
-            'transactionPin': '1234'
-        }
-    }.copy()
+  return {
+      'data': {
+          'fromUser': {
+              'userIdentifier': os.getenv('QMONEY_PAYER'),
+          },
+          'toUser': {
+              'userIdentifier': os.getenv('QMONEY_PAYEE'),
+          },
+          'serviceId': 'MOBILE_MONEY',
+          'productId': 'NHIA_GETMONEY',
+          'remarks': 'add',
+          'payment': [
+              {
+                  'amount': 1
+              },
+          ],
+          'transactionPin': os.getenv('QMONEY_PAYEE_PIN_CODE')
+      }
+  }.copy()
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope='class')
 def qmoney_access_token(qmoney_url, qmoney_credentials, qmoney_token):
-    return QMoney.login(qmoney_url, qmoney_credentials, qmoney_token)
+  return QMoney.login(qmoney_url, qmoney_credentials, qmoney_token)
+
+
+@pytest.fixture(scope='class')
+def gmail_client():
+  client = Gmail()
+  gmail_mark_as_read_recent_emails_with_qmoney_otp(client)
+  yield client
+  gmail_mark_as_read_recent_emails_with_qmoney_otp(client)
--- qmoney_payment/tests/test_qmoney_api_getmoney.py
+++ qmoney_payment/tests/test_qmoney_api_getmoney.py
@@ -3,109 +3,104 @@ import pprint
 import pytest
 import requests
 
-from helpers import QMoneyBearerAuth, set_into, del_from
+from helpers import QMoneyBearerAuth, QMoney, set_into, del_from
 
 
 class TestQmoneyAPIGetMoney:
 
-    def test_initiating_transaction(self, qmoney_access_token, qmoney_url,
-                                    qmoney_getmoney_json_payload):
-        json_payload = qmoney_getmoney_json_payload
+  def test_initiating_transaction(self, qmoney_access_token, qmoney_url,
+                                  qmoney_payer, qmoney_payee,
+                                  qmoney_payee_pin_code):
+    amount = 1
+    response = QMoney.initiate_transaction(qmoney_url, qmoney_access_token,
+                                           qmoney_payer, qmoney_payee, amount,
+                                           qmoney_payee_pin_code)
+    assert response.status_code == 200
+    json_response = response.json()
+    assert json_response['responseCode'] == '1'
+    assert json_response['responseMessage'] == 'OTP Send Successfully'
+    assert json_response['data']['transactionId'] is not None
 
-        response = requests.post(url=f'{qmoney_url}/getMoney',
-                                 json=json_payload,
-                                 auth=QMoneyBearerAuth(qmoney_access_token))
-        assert response.status_code == 200
-        json_response = response.json()
-        assert json_response['responseCode'] == '1'
-        assert json_response['responseMessage'] == 'OTP Send Successfully'
-        assert json_response['data']['transactionId'] is not None
+  def test_failing_at_initiating_transaction_when_missing_access_token(
+      self, qmoney_url, qmoney_credentials, qmoney_token,
+      qmoney_getmoney_json_payload):
+    json_payload = qmoney_getmoney_json_payload
 
-    def test_failing_at_initiating_transaction_when_missing_access_token(
-            self, qmoney_url, qmoney_credentials, qmoney_token,
-            qmoney_getmoney_json_payload):
-        json_payload = qmoney_getmoney_json_payload
+    response = requests.post(url=f'{qmoney_url}/getMoney', json=json_payload)
+    assert response.status_code == 401
+    json_response = response.json()
+    assert json_response['error'] == 'unauthorized'
+    assert json_response[
+        'error_description'] == 'Full authentication is required to access this resource'
 
-        response = requests.post(url=f'{qmoney_url}/getMoney',
-                                 json=json_payload)
-        assert response.status_code == 401
-        json_response = response.json()
-        assert json_response['error'] == 'unauthorized'
-        assert json_response[
-            'error_description'] == 'Full authentication is required to access this resource'
+  def test_failing_at_initiating_transaction_when_wrong_access_token(
+      self, qmoney_url, qmoney_payer, qmoney_payee, qmoney_payee_pin_code):
 
-    def test_failing_at_initiating_transaction_when_wrong_access_token(
-            self, qmoney_url, qmoney_credentials, qmoney_token,
-            qmoney_getmoney_json_payload):
-        json_payload = qmoney_getmoney_json_payload
+    amount = 1
+    fake_access_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dnaW5nQXMiOm51bGwsImF1ZCI6WyJBZGFwdGVyX09hdXRoX1Jlc291cmNlX1NlcnZlciJdLCJncmFudF90eXBlIjoicGFzc3dvcmQiLCJkZXZpY2VVbmlxdWVJZCI6bnVsbCwidXNlcl9uYW1lIjoiMTQwMDE1MDIiLCJzY'
+    response = QMoney.initiate_transaction(qmoney_url, fake_access_token,
+                                           qmoney_payer, qmoney_payee, amount,
+                                           qmoney_payee_pin_code)
+    assert response.status_code == 401
+    json_response = response.json()
+    assert json_response['error'] == 'invalid_token'
+    assert json_response[
+        'error_description'] == 'Cannot convert access token to JSON'
 
-        response = requests.post(
-            url=f'{qmoney_url}/getMoney',
-            json=json_payload,
-            auth=QMoneyBearerAuth(
-                'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dnaW5nQXMiOm51bGwsImF1ZCI6WyJBZGFwdGVyX09hdXRoX1Jlc291cmNlX1NlcnZlciJdLCJncmFudF90eXBlIjoicGFzc3dvcmQiLCJkZXZpY2VVbmlxdWVJZCI6bnVsbCwidXNlcl9uYW1lIjoiMTQwMDE1MDIiLCJzY'
-            ))
-        assert response.status_code == 401
-        json_response = response.json()
-        assert json_response['error'] == 'invalid_token'
-        assert json_response[
-            'error_description'] == 'Cannot convert access token to JSON'
+  @pytest.mark.parametrize("input_parameter",
+                           [(['data', 'fromUser', 'userIdentifier']),
+                            (['data', 'toUser', 'userIdentifier']),
+                            (['data', 'serviceId']), (['data', 'productId']),
+                            (['data', 'remarks']), (['data', 'payment']),
+                            (['data', 'transactionPin'])])
+  @pytest.mark.skip(
+      reason='Expecting a failure but Qmoney actually accepts and sends an OTP'
+  )
+  def test_failing_at_initiating_transaction_when_missing_input_parameter(
+      self, qmoney_access_token, qmoney_url, qmoney_getmoney_json_payload,
+      input_parameter):
+    json_payload = qmoney_getmoney_json_payload
 
-    @pytest.mark.parametrize("input_parameter",
-                             [(['data', 'fromUser', 'userIdentifier']),
-                              (['data', 'toUser', 'userIdentifier']),
-                              (['data', 'serviceId']), (['data', 'productId']),
-                              (['data', 'remarks']), (['data', 'payment']),
-                              (['data', 'transactionPin'])])
-    @pytest.mark.skip(
-        reason=
-        'Expecting a failure but Qmoney actually accepts and sends an OTP')
-    def test_failing_at_initiating_transaction_when_missing_input_parameter(
-            self, qmoney_access_token, qmoney_url,
-            qmoney_getmoney_json_payload, input_parameter):
-        json_payload = qmoney_getmoney_json_payload
+    del_from(json_payload, input_parameter)
 
-        del_from(json_payload, input_parameter)
+    response = requests.post(url=f'{qmoney_url}/getMoney',
+                             json=json_payload,
+                             auth=QMoneyBearerAuth(qmoney_access_token))
+    assert response.status_code == 200
+    assert False
 
-        response = requests.post(url=f'{qmoney_url}/getMoney',
-                                 json=json_payload,
-                                 auth=QMoneyBearerAuth(qmoney_access_token))
-        assert response.status_code == 200
-        assert False
+  @pytest.mark.parametrize("input_parameter",
+                           [(['data', 'fromUser', 'userIdentifier']),
+                            (['data', 'toUser', 'userIdentifier']),
+                            (['data', 'serviceId']), (['data', 'productId']),
+                            (['data', 'remarks']), (['data', 'payment']),
+                            (['data', 'transactionPin'])])
+  @pytest.mark.skip(
+      reason='Expecting a failure but Qmoney actually accepts and sends an OTP'
+  )
+  def test_failing_at_initiating_transaction_when_wrong_input_parameter(
+      self, qmoney_access_token, qmoney_url, qmoney_getmoney_json_payload,
+      input_parameter):
+    json_payload = qmoney_getmoney_json_payload
 
-    @pytest.mark.parametrize("input_parameter",
-                             [(['data', 'fromUser', 'userIdentifier']),
-                              (['data', 'toUser', 'userIdentifier']),
-                              (['data', 'serviceId']), (['data', 'productId']),
-                              (['data', 'remarks']), (['data', 'payment']),
-                              (['data', 'transactionPin'])])
-    @pytest.mark.skip(
-        reason=
-        'Expecting a failure but Qmoney actually accepts and sends an OTP')
-    def test_failing_at_initiating_transaction_when_wrong_input_parameter(
-            self, qmoney_access_token, qmoney_url,
-            qmoney_getmoney_json_payload, input_parameter):
-        json_payload = qmoney_getmoney_json_payload
+    set_into(json_payload, input_parameter, input_parameter[-1])
 
-        set_into(json_payload, input_parameter, input_parameter[-1])
+    response = requests.post(url=f'{qmoney_url}/getMoney',
+                             json=json_payload,
+                             auth=QMoneyBearerAuth(qmoney_access_token))
+    assert response.status_code == 200
+    assert False
 
-        response = requests.post(url=f'{qmoney_url}/getMoney',
-                                 json=json_payload,
-                                 auth=QMoneyBearerAuth(qmoney_access_token))
-        assert response.status_code == 200
-        assert False
+  @pytest.mark.skip(
+      reason='Expecting a failure but Qmoney actually accepts and sends an OTP'
+  )
+  def test_failing_at_initiating_transaction_when_the_payload_is_empty(
+      self, qmoney_access_token, qmoney_url, qmoney_getmoney_json_payload):
+    json_payload = qmoney_getmoney_json_payload
+    del json_payload['data']
 
-    @pytest.mark.skip(
-        reason=
-        'Expecting a failure but Qmoney actually accepts and sends an OTP')
-    def test_failing_at_initiating_transaction_when_the_payload_is_empty(
-            self, qmoney_access_token, qmoney_url,
-            qmoney_getmoney_json_payload):
-        json_payload = qmoney_getmoney_json_payload
-        del json_payload['data']
-
-        response = requests.post(url=f'{qmoney_url}/getMoney',
-                                 json=json_payload,
-                                 auth=QMoneyBearerAuth(qmoney_access_token))
-        assert response.status_code == 200
-        assert False
+    response = requests.post(url=f'{qmoney_url}/getMoney',
+                             json=json_payload,
+                             auth=QMoneyBearerAuth(qmoney_access_token))
+    assert response.status_code == 200
+    assert False
